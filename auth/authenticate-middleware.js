e8089ce0f19228089024ae1812f620d6a827c763
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-01-03 12:55:39 -0500

    created an auth middleware as well as auth router to allow for user creations

---
+++ auth/authenticate-middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (res, req, next) => {
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || "keep me secret";
+    jwt.verify(authorization, secret, function(err, decoded) {
+      if (err) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.token = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please login and try again. " });
+  }
+};
