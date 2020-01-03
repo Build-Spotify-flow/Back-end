9bc88fae17e97fcf5b506149af4374e0dc8bd07b
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-01-03 13:01:21 -0500

    REGSITER AND LOGIN functionality is in working order returning a proper token and hashing the password.

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 // maybe cors and or helmet as well??
 
+// the below middleware is to be implemented when saved songs and such are created.
+// const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/auth", authRouter);
 
 module.exports = server;
