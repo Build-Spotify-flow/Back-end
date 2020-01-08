4c7c63cbd7082fab0ec13b208540cee899bc0340
Michael Jordan <Insurikai@gmail.com>
2020-01-08 13:38:30 -0800

    updated register endpoint to respond with a token, and api/track endpoint to respond with the users liked and disliked lists

--- tracks/track-router.js
+++ tracks/track-router.js
@@ -1,35 +1,40 @@
 const router = require('express').Router();
 const model = require('./tracks-model');
+const users = require('../users/users-model')
 
-router.post('/', (req, res) => {
-  console.log(req.token)
-  model.findLiked(req.token.username).then(re => {
-    console.log('liked')
-    console.log(re)
-  });
-  model.findDisliked(req.token.username).then(re => {
-    console.log('disliked')
-    console.log(re)
-  });
-  res.status(200).send({ message: 'ok' });
+router.get('/', (req, res) => {
+  users.findBy({username: req.token.username}).then(user => {
+    const id = user[0].id;
+    model.findLiked(id).then(liked => {
+      model.findDisliked(id).then(disliked => {
+        res.status(200).send({liked, disliked});
+      });
+    });
+  })
 });
 
 router.post('/like', (req, res) => {
-  model.addLike(req.token.username, req.body).then(() => {
-    model.removeDislike(req.token.username, req.body)
-    model.findLiked(req.token.username).then(re => {
-      res.status(200).send(re)
+  users.findBy({username: req.token.username}).then(user => {
+    const id = user[0].id;
+    model.addLike(id, req.body).then(() => {
+      model.removeDislike(id, req.body)
+      model.findLiked(id).then(re => {
+        res.status(200).send(re)
+      });
     });
-  });
+  })
 });
 
 router.post('/dislike', (req, res) => {
-  model.addDislike(req.token.username, req.body).then(() => {
-    model.removeLike(req.token.username, req.body)
-    model.findDisliked(req.token.username).then(re => {
-      res.status(200).send(re)
+  users.findBy({username: req.token.username}).then(user => {
+    const id = user[0].id;
+    model.addDislike(id, req.body).then(() => {
+      model.removeLike(id, req.body)
+      model.findDisliked(id).then(re => {
+        res.status(200).send(re)
+      });
     });
-  });
+  })
 });
 
 module.exports = router;
