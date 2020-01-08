a96ab92a20d51d5c9b95a8310851444659f063d5
Michael Jordan <Insurikai@gmail.com>
2020-01-08 13:20:39 -0800

    removed redundant endpoints

--- tracks/track-router.js
+++ tracks/track-router.js
@@ -14,36 +14,22 @@ router.post('/', (req, res) => {
   res.status(200).send({ message: 'ok' });
 });
 
-router.post('/add/like', (req, res) => {
+router.post('/like', (req, res) => {
   model.addLike(req.token.username, req.body).then(() => {
+    model.removeDislike(req.token.username, req.body)
     model.findLiked(req.token.username).then(re => {
       res.status(200).send(re)
     });
   });
 });
 
-router.post('/add/dislike', (req, res) => {
+router.post('/dislike', (req, res) => {
   model.addDislike(req.token.username, req.body).then(() => {
+    model.removeLike(req.token.username, req.body)
     model.findDisliked(req.token.username).then(re => {
       res.status(200).send(re)
     });
   });
 });
-
-router.post('/remove/like', (req, res) => {
-  model.removeLike(req.token.username, req.body).then(() => {
-    model.findLiked(req.token.username).then(re => {
-      res.status(200).send(re)
-    });
-  });
-})
-
-router.post('/remove/dislike', (req, res) => {
-  model.removeDislike(req.token.username, req.body).then(() => {
-    model.findDisliked(req.token.username).then(re => {
-      res.status(200).send(re)
-    });
-  });
-})
 
 module.exports = router;
